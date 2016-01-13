8506ccaf47852d96960643a49e948f3bbb65164d
Vlad Ovinnikov <vlad.ovinnikov@gmail.com>
2016-01-13 11:19:00 +0100

    add current users command

--- server.js
+++ server.js
@@ -12,12 +12,34 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+function sendCurrentUsers(socket) {
+    var info = clientInfo[socket.id];
+    var users = [];
+
+    if(typeof info === 'undefined')
+        return;
+
+    Object.keys(clientInfo).forEach(function(socketId){
+        var userInfo = clientInfo[socketId];
+
+        if(info.room === userInfo.room){
+            users.push(userInfo.name);
+        }
+    });
+
+    socket.emit('message', {
+        name: 'System',
+        text: 'Current users: ' + users.join(', '),
+        timestamp: moment().valueOf()
+    })
+}
+
 io.on('connection', function (socket) {
     console.log('User connected via socket.io!');
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         var userData = clientInfo[socket.id];
-        if(typeof userData.room !== 'undefined'){
+        if (typeof userData.room !== 'undefined') {
             socket.leave(userData.room);
             socket.broadcast.to(userData.room).emit('message', {
                 name: 'System',
@@ -28,7 +50,7 @@ io.on('connection', function (socket) {
         }
     });
 
-    socket.on('joinRoom', function(req) {
+    socket.on('joinRoom', function (req) {
         clientInfo[socket.id] = req;
         socket.join(req.room);
         socket.broadcast.to(req.room).emit('message', {
@@ -41,8 +63,12 @@ io.on('connection', function (socket) {
     socket.on('message', function (message) {
         console.log('Message received: ' + message.text);
 
-        message.timestamp = moment().valueOf();
-        io.to(clientInfo[socket.id].room).emit('message', message);
+        if (message.text === '@currentUsers') {
+            sendCurrentUsers(socket);
+        } else {
+            message.timestamp = moment().valueOf();
+            io.to(clientInfo[socket.id].room).emit('message', message);
+        }
     });
 
     socket.emit('message', {
